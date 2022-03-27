e175dc071fe1e587584bfaa26ddbd130ade4f963
SwatKat1977 <paulmorriss1977@gmail.com>
2022-03-27 11:21:48 +0100

    basic configuration for ingestor

---
+++ stars/ingestor/configuration.py
@@ -0,0 +1,36 @@
+'''
+Copyright 2019-2021 Image Gopher
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+from config_base import ConfigBase
+
+# def __init__(self, config_file : str = None, file_required : bool = False,
+#              required_values = None):
+
+class Configuration(ConfigBase):
+    """ Class defining ingestor service configuration options """
+
+    required_items = {
+        'logging':
+        {
+            'log_level': ('DEBUG', 'INFO')
+        },
+        'general':
+        {
+            'import_directory': None
+        }
+    }
+
+    def __init__(self, config_file, file_required : bool) -> None:
+        super().__init__(config_file, file_required, self.required_items)
