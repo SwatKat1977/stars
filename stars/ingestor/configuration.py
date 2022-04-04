6503c071ad7629e4cc269d88d7d4efc3f72eb125
SwatKat1977 <paulmorriss1977@gmail.com>
2022-04-04 21:51:31 +0100

    correct copyright notice

--- stars/ingestor/configuration.py
+++ stars/ingestor/configuration.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2019-2021 Image Gopher
+Copyright 2022 Staff Recruitment System (STARS)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
